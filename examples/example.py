9ef8396439972115b043edcd9636825f1fea6006
ChayaSt <chaya.stern@choderalab.org>
2016-12-05 16:09:20 -0500

    updated example

--- examples/example.py
+++ examples/example.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 
 # Generate random trajectory
 sample_1 = np.random.lognormal(1.0, 0.1, size=1000)
-sample_2 = np.random.lognormal(0.97, 0.1, size=500)
-sample_3 = np.random.lognormal(1.03, 0.1, size=1000)
+sample_2 = np.random.lognormal(0.95, 0.1, size=500)
+sample_3 = np.random.lognormal(1.05, 0.1, size=1000)
 
 sample = np.concatenate((sample_1, sample_2, sample_3, sample_1))
 
-detector = cpDetector([sample], distribution='log_normal')
+detector = cpDetector([sample], distribution='log_normal', log_odds_threshold=-10)
 detector.detect_cp()
 
 plt.plot(sample)
 for i in range(len(detector.change_points['traj_0']['ts'])):
-    if detector.change_points['traj_0']['log_odds'][i] > 20:
+    if detector.change_points['traj_0']['log_odds'][i] > 0:
         plt.axvline(detector.change_points['traj_0']['ts'][i], color='r')
     else:
         plt.axvline(detector.change_points['traj_0']['ts'][i], color='g')
