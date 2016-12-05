e229d569edcee07b4c9bb1689752665b6405bd29
ChayaSt <chaya.stern@choderalab.org>
2016-12-05 16:05:49 -0500

    fixed tests

--- tests/test_cp_detector.py
+++ tests/test_cp_detector.py
@@ -4,14 +4,15 @@ from cpdetect import cpDetector
 from cpdetect.cp_detector import (Normal, LogNormal)
 import numpy as np
 import unittest
-from mpmath import mpf, gamma, log
 import pandas as pd
+from scipy.special import gammaln
 
 data = np.load('files/data.npy')
 data_2 = np.load('files/data2.npz')
 
 # Convert to cpDetect format
 data_2 = [data_2[i] for i in data_2.files]
+detector = cpDetector(data_2, distribution='log_normal', log_odds_threshold=-10)
 
 
 class TestCpDetect(unittest.TestCase):
@@ -36,19 +37,15 @@ class TestCpDetect(unittest.TestCase):
         self.assertEqual(detector.distribution, 'log_normal')
         self.assertEqual(detector.nobservations, 2)
         self.assertEqual(detector.observation_lengths, [2500, 3000])
-        self.assertEqual(detector.threshold, 0)
-
-    def test_gamma(self):
-        """ Test gamma table """
-
-        gammas = [-99, -99, -99]
-        for i in range(3, max(detector.observation_lengths) + 1):
-            gammas.append(gamma(0.5*i - 1))
-
-        self.assertEqual(gammas, detector.gamma)
+        self.assertEqual(detector.threshold, -10)
 
     def test_log_gamma(self):
         """ Test log gamma """
+        gammas = [-99, -99, -99]
+        for i in range(3, max(detector.observation_lengths) + 1):
+            gammas.append(gammaln(0.5*i - 1))
+
+        self.assertEqual(gammas, detector.loggamma)
 
     def test_split(self):
         """ Test split """
@@ -57,19 +54,19 @@ class TestCpDetect(unittest.TestCase):
         """ test log odds calculation and finding ts """
         t, log_odds = detector._normal_lognormal_bf(data)
         self.assertEqual(t, 1436)
-        self.assertEqual(log_odds, 24.262575826612192)
+        self.assertEqual(log_odds, 5.1818026662176635)
 
     def test_cpdetect(self):
         """ Test the detector """
 
         detector.detect_cp()
 
-        data = {'ts':[1500., 1019.], 'log_odds': [95.5929931403725, 7.62963882900431],
+        data = {'ts':[1500., 1019.], 'log_odds': [21.322622, -6.867833],
                 'start_end': [(0, 2500), (0, 1500)]}
         df = pd.DataFrame(data, columns=['ts', 'log_odds', 'start_end'])
         self.assertTrue(df['ts'].equals(detector.change_points['traj_0']['ts']))
         self.assertTrue(df['start_end'].equals(detector.change_points['traj_0']['start_end']))
-        self.assertAlmostEqual(df['log_odds'][0], detector.change_points['traj_0']['log_odds'][0])
+        self.assertAlmostEqual(df['log_odds'][0], detector.change_points['traj_0']['log_odds'][0], 6)
         self.assertTrue(len(detector.change_points['traj_0']), 2)
         self.assertTrue(len(detector.change_points['traj_1']), 3)
 
